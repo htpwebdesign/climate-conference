580b04b7ae09334fc2c5fcc855423deef1c800df
nikia123 <nikia@nshaw.dev>
2023-06-27 16:15:44 -0700

    created industry taxonomy. front-page and page-faq added and their respective template parts

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -52,3 +52,49 @@
 //     register_post_type('fwd-work', $args);
 // }
 // add_action('init', 'climate_register_custom_post_types');
+
+
+
+//Register Taxonomies Function
+function ccc_register_taxonomies() {
+
+    /**
+    * Register an 'industry' taxonomy for post types: 'conference-events', 'conference-speakers', 'conference-sponsors'.
+    * Used on home page and for filtering on schedule and speakers pages by industry.
+    */
+
+    $labels = array(
+        'name'              => _x( 'Industry Types', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Industry Type', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Industry Types' ),
+        'all_items'         => __( 'All Industry Types' ),
+        'parent_item'       => __( 'Parent Industry Type' ),
+        'parent_item_colon' => __( 'Parent Industry Type:' ),
+        'edit_item'         => __( 'Edit Industry Type' ),
+        'update_item'       => __( 'Update Industry Type' ),
+        'add_new_item'      => __( 'Add New Industry Type' ),
+        'new_item_name'     => __( 'New Industry Type Name' ),
+        'menu_name'         => __( 'Industry Type' ),
+    );
+
+    $args = array(
+        'hierarchical'          => true,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'industry' ),
+    );
+
+
+    register_taxonomy( 'conference-industry-type', array( 'conference-events', 'conference-speakers', 'conference-sponsors' ), $args );
+
+}
+
+add_action('init', 'ccc_register_taxonomies');
+
+
+?>
