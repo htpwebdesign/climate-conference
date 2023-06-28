f27da2dd8bab447b85dd244e07fcfc13efc7c33a
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-06-28 10:56:29 -0700

    taxonomies and custom post types for my part complete

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -1,80 +1,81 @@
 <?php
-// function climate_register_custom_post_types()
-// {
+function ccc_register_custom_post_types()
+{
+    // Register Events 
+    $labels = array(
+        'name'                  => _x('Events', 'post type general name'),
+        'singular_name'         => _x('Event', 'post type singular name'),
+        'menu_name'             => _x('Events', 'admin menu'),
+        'name_admin_bar'        => _x('Event', 'add new on admin bar'),
+        'add_new'               => _x('Add New', 'event'),
+        'add_new_item'          => __('Add New Event'),
+        'new_item'              => __('New Event'),
+        'edit_item'             => __('Edit Event'),
+        'view_item'             => __('View Event'),
+        'all_items'             => __('All Events'),
+        'search_items'          => __('Search Events'),
+        'parent_item_colon'     => __('Parent Events:'),
+        'not_found'             => __('No events found.'),
+        'not_found_in_trash'    => __('No events found in Trash.'),
+        'archives'              => __('Event Archives'),
+        'insert_into_item'      => __('Insert into event'),
+        'uploaded_to_this_item' => __('Uploaded to this event'),
+        'filter_item_list'      => __('Filter events list'),
+        'items_list_navigation' => __('event list navigation'),
+        'items_list'            => __('Events list'),
+        'featured_image'        => __('Event featured image'),
+        'set_featured_image'    => __('Set event featured image'),
+        'remove_featured_image' => __('Remove event featured image'),
+        'use_featured_image'    => __('Use as featured image'),
+    );
 
-//     // Register Works
-//     $labels = array(
-//         'name'                  => _x('Works', 'post type general name'),
-//         'singular_name'         => _x('Work', 'post type singular name'),
-//         'menu_name'             => _x('Works', 'admin menu'),
-//         'name_admin_bar'        => _x('Work', 'add new on admin bar'),
-//         'add_new'               => _x('Add New', 'work'),
-//         'add_new_item'          => __('Add New Work'),
-//         'new_item'              => __('New Work'),
-//         'edit_item'             => __('Edit Work'),
-//         'view_item'             => __('View Work'),
-//         'all_items'             => __('All Works'),
-//         'search_items'          => __('Search Works'),
-//         'parent_item_colon'     => __('Parent Works:'),
-//         'not_found'             => __('No works found.'),
-//         'not_found_in_trash'    => __('No works found in Trash.'),
-//         'archives'              => __('Work Archives'),
-//         'insert_into_item'      => __('Insert into work'),
-//         'uploaded_to_this_item' => __('Uploaded to this work'),
-//         'filter_item_list'      => __('Filter works list'),
-//         'items_list_navigation' => __('Works list navigation'),
-//         'items_list'            => __('Works list'),
-//         'featured_image'        => __('Work featured image'),
-//         'set_featured_image'    => __('Set work featured image'),
-//         'remove_featured_image' => __('Remove work featured image'),
-//         'use_featured_image'    => __('Use as featured image'),
-//     );
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'schedule'), // may need to change to work if doesn't work 
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-archive',
+        'supports'           => array('title', 'thumbnail', 'editor'),
+    );
 
-//     $args = array(
-//         'labels'             => $labels,
-//         'public'             => true,
-//         'publicly_queryable' => true,
-//         'show_ui'            => true,
-//         'show_in_menu'       => true,
-//         'show_in_nav_menus'  => true,
-//         'show_in_admin_bar'  => true,
-//         'show_in_rest'       => true,
-//         'query_var'          => true,
-//         'rewrite'            => array('slug' => 'works'),
-//         'capability_type'    => 'post',
-//         'has_archive'        => true,
-//         'hierarchical'       => false,
-//         'menu_position'      => 5,
-//         'menu_icon'          => 'dashicons-archive',
-//         'supports'           => array('title', 'thumbnail', 'editor'),
-//     );
-
-//     register_post_type('fwd-work', $args);
-// }
-// add_action('init', 'climate_register_custom_post_types');
+    register_post_type('ccc-events', $args);
+}
+add_action('init', 'ccc_register_custom_post_types');
 
 
 
 //Register Taxonomies Function
-function ccc_register_taxonomies() {
+function ccc_register_taxonomies()
+{
 
     /**
-    * Register an 'industry' taxonomy for post types: 'conference-events', 'conference-speakers', 'conference-sponsors'.
-    * Used on home page and for filtering on schedule and speakers pages by industry.
-    */
+     * Register an 'industry' taxonomy for post types: 'conference-events', 'conference-speakers', 'conference-sponsors'.
+     * Used on home page and for filtering on schedule and speakers pages by industry.
+     */
 
+    // Industry Types
     $labels = array(
-        'name'              => _x( 'Industry Types', 'taxonomy general name' ),
-        'singular_name'     => _x( 'Industry Type', 'taxonomy singular name' ),
-        'search_items'      => __( 'Search Industry Types' ),
-        'all_items'         => __( 'All Industry Types' ),
-        'parent_item'       => __( 'Parent Industry Type' ),
-        'parent_item_colon' => __( 'Parent Industry Type:' ),
-        'edit_item'         => __( 'Edit Industry Type' ),
-        'update_item'       => __( 'Update Industry Type' ),
-        'add_new_item'      => __( 'Add New Industry Type' ),
-        'new_item_name'     => __( 'New Industry Type Name' ),
-        'menu_name'         => __( 'Industry Type' ),
+        'name'              => _x('Industry Types', 'taxonomy general name'),
+        'singular_name'     => _x('Industry Type', 'taxonomy singular name'),
+        'search_items'      => __('Search Industry Types'),
+        'all_items'         => __('All Industry Types'),
+        'parent_item'       => __('Parent Industry Type'),
+        'parent_item_colon' => __('Parent Industry Type:'),
+        'edit_item'         => __('Edit Industry Type'),
+        'update_item'       => __('Update Industry Type'),
+        'add_new_item'      => __('Add New Industry Type'),
+        'new_item_name'     => __('New Industry Type Name'),
+        'menu_name'         => __('Industry Type'),
     );
 
     $args = array(
@@ -86,15 +87,75 @@ function ccc_register_taxonomies() {
         'show_in_rest'          => true,
         'show_admin_column'     => true,
         'query_var'             => true,
-        'rewrite'               => array( 'slug' => 'industry' ),
+        'rewrite'               => array('slug' => 'industry'),
     );
 
 
-    register_taxonomy( 'conference-industry-type', array( 'conference-events', 'conference-speakers', 'conference-sponsors' ), $args );
+    register_taxonomy('conference-industry-type', array('conference-events', 'conference-speakers', 'conference-sponsors'), $args);
 
+
+    // Event Type 
+    $labels = array(
+        'name'              => _x('Event Types', 'taxonomy general name'),
+        'singular_name'     => _x('Event Type', 'taxonomy singular name'),
+        'search_items'      => __('Search Event Types'),
+        'all_items'         => __('All Event Types'),
+        'parent_item'       => __('Parent Event Type'),
+        'parent_item_colon' => __('Parent Event Type:'),
+        'edit_item'         => __('Edit Event Type'),
+        'update_item'       => __('Update Event Type'),
+        'add_new_item'      => __('Add New Event Type'),
+        'new_item_name'     => __('New Event Type Name'),
+        'menu_name'         => __('Event Type'),
+    );
+
+    $args = array(
+        'hierarchical'          => true,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array('slug' => 'event-types'),
+    );
+
+
+    register_taxonomy('conference-event-type', array('conference-events'), $args);
+
+
+    // Schedule 
+    $labels = array(
+        'name'              => _x('Event', 'taxonomy general name'),
+        'singular_name'     => _x('Event', 'taxonomy singular name'),
+        'search_items'      => __('Search Event'),
+        'all_items'         => __('All Event'),
+        'parent_item'       => __('Parent Event'),
+        'parent_item_colon' => __('Parent Event:'),
+        'edit_item'         => __('Edit Event'),
+        'update_item'       => __('Update Event'),
+        'add_new_item'      => __('Add New Event'),
+        'new_item_name'     => __('New Event Name'),
+        'menu_name'         => __('Event'),
+    );
+
+    $args = array(
+        'hierarchical'          => true,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array('slug' => 'schedule'),
+    );
+
+
+    register_taxonomy('conference-event-taxonomy', array('conference-events'), $args);
 }
 
+
+
 add_action('init', 'ccc_register_taxonomies');
-
-
-?>
