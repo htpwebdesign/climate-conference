c5e5b22b3729dc5143a6feef086fd1d5244ce1df
BrookeNesvog <122934106+BrookeNesvog@users.noreply.github.com>
2023-07-06 15:58:52 -0700

    changed title

--- single-conference-speakers.php
+++ single-conference-speakers.php
@@ -19,30 +19,42 @@ get_header();
 
         $job_title_and_company = get_field('job_title_and_company');
         $portrait_id = get_field('portrait_', false, false);
+        $speaker_name = esc_html(get_the_title());
         $speaker_info = get_field('speaker_info');
 
+        // Display speaker name and job title and company
+        if ($speaker_name || $job_title_and_company) {
+            echo '<div class="speaker-info">';
+            echo '<div class="job-title-and-company">';
+            if ($speaker_name) {
+                echo '<h1>' . $speaker_name . '</h1>';
+            }
+            if ($job_title_and_company) {
+                echo '<h2>' . $job_title_and_company . '</h2>';
+            }
+            echo '</div>';
+        }
+
     ?>
-        <div class="speaker-info">
-            <?php if ($portrait_id) : ?>
-                <div class="portrait">
-                    <?php echo wp_get_attachment_image($portrait_id, 'full'); ?>
-                </div>
-            <?php endif; ?>
+        <?php if ($portrait_id) : ?>
+            <div class="portrait">
+                <?php echo wp_get_attachment_image($portrait_id, 'full'); ?>
+            </div>
+        <?php endif; ?>
 
-            <?php if ($job_title_and_company) : ?>
-                <div class="job-title-and-company">
-                    <p><?php echo $job_title_and_company; ?></p>
-                </div>
-            <?php endif; ?>
+        <?php if ($speaker_info) : ?>
+    <p class="speaker-info-acf">
+        <?php echo $speaker_info; ?>
+    </p>
+<?php endif; ?>
 
-            <?php if ($speaker_info) : ?>
-                <div class="speaker-info-acf">
-                    <?php echo $speaker_info; ?>
-                </div>
-            <?php endif; ?>
-        </div>
 
-    <?php
+        <?php
+        // Close speaker-info div
+        if ($speaker_name || $job_title_and_company) {
+            echo '</div>';
+        }
+
         // Display speaker content
         get_template_part('template-parts/content', 'single');
 
